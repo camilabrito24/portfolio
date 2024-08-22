ef371dadef2395b55b0889faefba9426b588139a
Camila Brito <camilam.brito@outlook.com.br>
2024-08-22 15:07:34 -0300

    N01_F01 - Criando main e footer da pagina home.

--- index.php
+++ index.php
@@ -25,7 +25,27 @@
             </button>
         </nav>
     </header>
-    <main></main>
-    <footer></footer>
+    <main class="container">
+        <section class="container-texto">
+            <h1 class="container-texto-titulo">Olá, eu sou a Camila</h1>
+            <p class="container-texto-paragrafo-destaque">Designer e Desenvolvedora Frontend / Backend.</p>
+            <p class="container-texto-paragrafo">Espero que através deste portfólio possa vir a me conhecer e conhecer o meu trabalho.
+                Caso queira entrar em contato comigo para maiores informações basta clicar no botão logo abaixo:</p>
+            <a href="." class="container-texto-btn">
+                CONTATE-ME
+            </a>
+        </section>
+        <section class="container-imagem">
+            <img src="src/BANNER.png">
+        </section>
+    </main>
+    <footer class="rodape">
+        <p class="rodape-paragrafo-destaque">
+            © Copyright 2024
+        </p>
+        <p class="rodape-paragrafo">
+            Feito por Camila Brito
+        </p>
+    </footer>
 </body>
 </html>
