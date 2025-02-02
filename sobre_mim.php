6a3ac59c61f0ec11d03facfecb8ced0e3fb51da2
Camila Brito <camilam.brito@outlook.com.br>
2025-02-02 15:33:05 -0300

    N01_F03 - Ajustando informações da pagina sobre mim.

--- sobre_mim.php
+++ sobre_mim.php
@@ -31,14 +31,53 @@
                 EXPERIÊNCIA PROFISSIONAL
             </button>
             <button class="btn_controle">
-                FORMAÇÃO ACADEMICA
+                FORMAÇÃO ACADÊMICA
             </button>
             <button class="btn_controle">
                 HABILIDADES
             </button>
         </section>
-        <section class="container_dados">
-
+        <section class="container-experiencia-profissional">
+            <section class="container-dados">
+                <h1 class="container-dados-titulo">Assistente de Operações
+                    2017 - 2021</h1>
+                <p class="container-dados-paragrafo-destaque">GRUPO NEW SPACE</p>
+                <ul class="container-dados-paragrafo">
+                    <li>Controle de qualidade.</li>
+                    <li>Ouvidoria.</li>
+                    <li>Regularização de Dados.</li>
+                    <li>Venda e formalização de documentos e contratos. </li>
+                    <li>Suporte ao cliente através do Whatsapp e telefone.  </li>
+                </ul>
+            </section>
+            <section class="container-dados">
+                <h1 class="container-dados-titulo">Estágio em Teste de Software
+                    2022 - 2023</h1>
+                <p class="container-dados-paragrafo-destaque">ZANTHUS</p>
+                <ul class="container-dados-paragrafo">
+                    <li>Teste de software unitário e integração.</li>
+                    <li>Teste automatizado em sistema PDV através do Testlink.</li>
+                </ul>
+            </section>
+            <section class="container-dados">
+                <h1 class="container-dados-titulo">Analista de Desenvolvimento de Software
+                    2023 - 2023</h1>
+                <p class="container-dados-paragrafo-destaque">ZANTHUS</p>
+                <ul class="container-dados-paragrafo">
+                    <li>Teste manual de software (unitário e integração).</li>
+                    <li>Teste automatizado em sistema Manager e PDV através do Testlink.</li>
+                </ul>
+            </section>
+            <section class="container-dados">
+                <h1 class="container-dados-titulo">Programador de Sistema da Informação
+                    2023 - Atualmente</h1>
+                <p class="container-dados-paragrafo-destaque">ZANTHUS</p>
+                <ul class="container-dados-paragrafo">
+                    <li>Correção de bugs no sistema através de PHP e Javascript.</li>
+                    <li>Realização de melhorias no sistema através de PHP e Javascript.</li>
+                    <li>Criação de novos processos no sistema através de PHP e Javascript.</li>
+                </ul>
+            </section>
         </section>
     </main>
     <footer class="rodape">
