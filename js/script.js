d7823735057893e7a7d4b97ed60f53914a13193f
Camila Brito <camilam.brito@outlook.com.br>
2025-05-26 21:16:54 -0300

    N01_F06 - Criando animação com gif.

--- js/script.js
+++ js/script.js
@@ -8,6 +8,9 @@ const containerDadosDestaque = document.querySelectorAll(".container-dados-parag
 const containerDadosTexto = document.querySelectorAll(".container-dados-paragrafo");
 const inputContato = document.querySelectorAll(".input-contato");
 var modoEscuro = false;
+var projetoImg = document.querySelectorAll(".container-projetos-card-img");
+
+this.animaProjeto(projetoImg);
 btnDarkMode.addEventListener('click', ()=>{
     var elemento = {};
     elemento.btndarkmode = btnDarkMode;
@@ -146,4 +149,26 @@ function alteraModoClaro(elemento){
             elemento.style.color = '#343131';
         })
     }
+}
+
+function animaProjeto(elemento){
+    elemento.forEach(elemento => {
+        elemento.addEventListener('mouseover', function (){
+            var caminhoImagem = [];
+            caminhoImagem.push(elemento.src);
+            caminhoImagem = caminhoImagem[0];
+            const novoCaminhoImg = caminhoImagem.replace("http://localhost/Projetos/portfolio/","./").replace(".png",".gif");
+            elemento.setAttribute('src', novoCaminhoImg);
+            elemento.style.width = '700px';
+        });
+
+        elemento.addEventListener('mouseout', function (){
+            var caminhoImagem = [];
+            caminhoImagem.push(elemento.src);
+            caminhoImagem = caminhoImagem[0];
+            const novoCaminhoImg = caminhoImagem.replace("http://localhost/Projetos/portfolio/","./").replace(".gif",".png");
+            elemento.setAttribute('src', novoCaminhoImg);
+            elemento.style.width = '350px';
+        });
+    })
 }
